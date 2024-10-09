7752d04ab2e55be9b223c8c8192c5cd18bde7505
Chonghow29 <125256306+Chonghow29@users.noreply.github.com>
2024-10-10 01:48:02 +0800

    Add Homework 10 folder

---
+++ homework_week10/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "homework_week_10.vg.json";
+vegaEmbed("#week10", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
