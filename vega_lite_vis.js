92e12dfb754e20d1344614af20da546f30025b2c
Chonghow29 <125256306+Chonghow29@users.noreply.github.com>
2024-10-10 01:48:43 +0800

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "homework_week_10.vg.json";
-vegaEmbed("#week10", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
