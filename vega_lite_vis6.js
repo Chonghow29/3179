08eaed32ab3cbd21431d5f3c7c8c68d894bc0f93
Chonghow29 <125256306+Chonghow29@users.noreply.github.com>
2024-10-13 15:03:03 +0800

    Add Data Visualization 2

---
+++ vega_lite_vis6.js
@@ -0,0 +1,5 @@
+var vg_6 = "json/freshwater_use_by_aggregated_region.vg.json";
+    
+vegaEmbed("#freshwater_use_by_aggregated_region", vg_6).then(function(result) {
+    // Access the Vega view instance
+}).catch(console.error);
